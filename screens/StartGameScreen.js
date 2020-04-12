191531d39b22fd3905adb8a393eae809112450f4
Franco Colmenarez <francoacg1@gmail.com>
2020-04-12 11:33:36 -0500

    Handling the reset button

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -22,6 +22,10 @@ const StartGameScreen = props => {
     setEnteredValue(inputText.replace(/[^0-9]/g, ''))
   }
 
+  const resetInputHandler = () => {
+    setEnteredValue('')
+  }
+
   return (
     <TouchableWithoutFeedback onPress={ () => { Keyboard.dismiss() } }>
       <View style={ styles.screen }>
@@ -41,7 +45,7 @@ const StartGameScreen = props => {
 
           <View style={ styles.buttonContainer }>
             <View style={ styles.button }>
-              <Button title="Reset" onPress={ () => {} } color={ Colors.accent } />
+              <Button title="Reset" onPress={ resetInputHandler } color={ Colors.accent } />
             </View>
             <View style={ styles.button }>
               <Button title="Confirm" onPress={ () => {} } color={ Colors.primary } />
