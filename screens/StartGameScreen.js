9271b5ee0e0bad4088f70011a1c3b9b9f407688c
Franco Colmenarez <francoacg1@gmail.com>
2020-04-12 10:26:13 -0500

    Creating the first screen of the game

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
+
+const StartGameScreen = props => {
+  return (
+    <View style={ styles.screen }>
+      <Text>Start a new game!</Text>
+      <View>
+        <Text>Select a number</Text>
+        <TextInput />
+
+        <View>
+          <Button title="Reset" onPress={ () => {} } />
+          <Button title="Confirm" onPress={ () => {} } />
+        </View>
+
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center',
+  }
+})
+
+export default StartGameScreen
